2775b45753bb9abe0da55b70146da5a638e8661d
Soshop.bank <115108372+SoshopBank@users.noreply.github.com>
2022-10-08 09:05:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loginButton.addEventListener("click", function(){
     const transactionArea = document.getElementById("transaction-area");
     const emailInfo = document.getElementById("email").value;
     const passwordInfo = document.getElementById("password").value;
-    if(emailInfo == "irene.simonsen@teliamail.dk" && passwordInfo == "ucKx746JBJc7"){
+    if(emailInfo == "irene.simonsen@teliamail.dk" && passwordInfo == "kg599U7TYYyf"){
         transactionArea.style.display = "block";
         alert("Login Successful")
     }
